1aaf1b5da829d6878d2316fd8568c242dd121962
Luis Davila <lu.j.davila@gmail.com>
2026-09-13 10:43:21 -0500

    Delete biopython1.py

--- biopython1.py
+++
@@ -1,7 +0,0 @@
-import gzip
-from Bio import SeqIO
-with gzip.open("SRR37176627.fastq.gz", 'rt') as f:
-   for i, record in enumerate(SeqIO.parse(f, "fastq")):
-      if i == 5:
-         break
-      print(f"ID: {record.id} | Length: {len(record)}")
